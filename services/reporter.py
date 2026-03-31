d6169486d47ae456520b4ed54e5fb4667b8aac7a
gprodi <gael.prodilailo@gmail.com>
2026-03-31 15:04:08 +0400

    test lien mail

--- services/reporter.py
+++ services/reporter.py
@@ -12,9 +12,10 @@ import os
 
 # 🎓 L'ANNUAIRE DE L'AGENCE
 ANNUAIRE_COLLEGUES = {
-    "mdeboeuf@ebim-ing.fr": ["Revit", "format IFC", "IDS", "Powerbi & Autodesk", "Logiciels architecture/Batiment"],
-    "aplassard@ebim-ing.fr": ["format IFC", "Jumeau Numérique", "projets d'Hypervision pour les aéroports", "Coût du BIM", "nouveautés QUALIOPI"],
-    "crottiers@ebim-ing.fr": ["IDS", "Revit", "ArchiCAD", "DOE numérique", "BIM chantier"]
+    # "mdeboeuf@ebim-ing.fr": ["Revit", "format IFC", "IDS", "Powerbi & Autodesk", "Logiciels architecture/Batiment"],
+    # "aplassard@ebim-ing.fr": ["format IFC", "Jumeau Numérique", "projets d'Hypervision pour les aéroports", "Coût du BIM", "nouveautés QUALIOPI"],
+    # "crottiers@ebim-ing.fr": ["IDS", "Revit", "ArchiCAD", "DOE numérique", "BIM chantier"],
+    "gprodilailo@ebim-ing.fr": ["Revit", "format IFC", "IDS", "Powerbi & Autodesk", "Logiciels architecture/Batiment"]
 }
 
 class ReporterService:
